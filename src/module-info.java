98f776feb226324d2163e63ea9fe7e258af76a1f
ViFLara <viviflara@gmail.com>
2021-04-29 08:59:45 -0300

    DepartmentForm (dialog) design

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module workshop {
 	requires javafx.fxml;
 	requires javafx.base;
 	requires java.sql;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
